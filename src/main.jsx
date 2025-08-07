d40fe369a93a8259278d78e87c003d084092ff71
hanna <hannawkozak@gmail.com>
2025-08-07 23:59:24 +0100

    feat: setting react query

--- src/main.jsx
+++ src/main.jsx
@@ -10,24 +10,40 @@ import { Dashboard } from './pages/Dashboard.jsx';
 import { Settings } from './pages/Settings.jsx';
 import { Users } from './pages/Users.jsx';
 import { PageNotFound } from './pages/PageNotFound.jsx';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 1,
+			staleTime: 1000 * 60,
+		},
+	},
+});
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route element={<App />}>
-					<Route index element={<Navigate replace to="dashboard" />} />
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="bookings" element={<Bookings />} />
-					<Route path="apartments" element={<Apartments />} />
-					<Route path="users" element={<Users />} />
-					<Route path="settings" element={<Settings />} />
-					<Route path="account" element={<Account />} />
-				</Route>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+			<BrowserRouter>
+				<Routes>
+					<Route element={<App />}>
+						<Route index element={<Navigate replace to="dashboard" />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="apartments" element={<Apartments />} />
+						<Route path="users" element={<Users />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="account" element={<Account />} />
+					</Route>
 
-				<Route path="login" element={<Login />} />
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	</StrictMode>
 );
